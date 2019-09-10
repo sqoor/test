11ff80d841a9441556f1952e4ed1c7979ebb0a2b
MoathG <gharibmoath@gmail.com>
2019-09-10 07:12:05 +0300

    new fork from moath pc

--- view/src/components/home/NewPost.js
+++ view/src/components/home/NewPost.js
@@ -1,11 +1,31 @@
 import React, { Component } from 'react'
 
 export class NewPost extends Component {
+
+    // submit = (e) => {
+    //     e.preventDefault();
+
+    //     const text = text.value;
+    //     const title = title.value;
+    //     console.log(text, title);
+
+    //     this.props.addPost({text, title})
+    // }
+
     render() {
         return (
-            <div>
-                NewPost Component
-            </div>
+                <div className="add_Post_form">
+                    <form onSubmit={this.submit}>
+                        <h4 className="text-center">Create New Post</h4>
+                        <input rel={elem => this.title = elem} type="text" className="form-control" placeholder="PostName" />
+                        <textarea
+                            rel={elem => this.text = elem}
+                            className="form-control"
+                            placeholder="Enter Your Code Here"
+                        ></textarea>
+                        <button className="btn btn-info">Publish Your Question</button>
+                    </form>
+                </div>
         )
     }
 }
--- view/src/components/home/PostItem.js
+++ view/src/components/home/PostItem.js
@@ -1,11 +1,44 @@
 import React, { Component } from 'react'
 
 export class Post extends Component {
+  state = {
+    
+  }
+
     render() {
+
+      // const { text, label } = this.props.post
         return (
-            <div>
-                Post Component
-            </div>
+          <div>
+            <div className="post_item">
+              <div className="User_info">
+                <div className="user_img">
+                  <img src="./images/user.png" />
+                </div>
+                <div className="user_name">Ahmad Alaarg</div>
+                <span className="userinfo_date">
+                  <b>Date:</b> 12/12/2019
+                </span>
+                <span className="userinfo_date">
+                  <b>time:</b> 12:83 PM
+                </span>
+              </div>
+              <div className="post">
+                <h4>'dasfasfafasf'</h4>
+                <textarea
+                  disabled="true"
+                  value='dasdasdasdasdasd'
+                >
+                </textarea>
+              </div>
+              <div className="pos_anser">
+                  <div className="anser_count">30 Comment</div>
+                <button className="btn btn-info">
+                    Answer
+                </button>
+              </div>
+            </div> 
+          </div>
         )
     }
 }
