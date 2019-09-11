4fa713e1fc8c06b389109c3cc3d07aeac0ed1169
alaarg <ahmadalaarg123@gmail.com>
2019-09-11 16:20:37 +0300

    f git commit -m f

--- view/src/components/profile/MyProfile.js
+++ view/src/components/profile/MyProfile.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import MyInfo from "./MyInfo";
 import MyPostsList from "./MyPostsList";
-import NewPost from '../home/NewPost'
+import NewPost from "../home/NewPost";
 import axios from "axios";
 
 export class MyProfile extends Component {
@@ -13,7 +13,7 @@ export class MyProfile extends Component {
       email: "asma@gmail.com",
       phone: 123456789,
       password: "1234",
-      image: "url",
+      image: "https://i.ibb.co/PD7MsCm/user.png",
       __v: 0
     },
     myPosts: []
@@ -56,9 +56,49 @@ export class MyProfile extends Component {
 
     return (
       <div>
-        <MyInfo myInfo={myInfo} />
-        <NewPost/>
-        <MyPostsList myPosts={myPosts} deletePost={deletePost} />
+        <div className="min_header"></div>
+        <div className="container">
+          <div className="row">
+            <div className="col-md-10 offset-md-1">
+              <div
+                className="User_profile text-center "
+                style={{
+                  marginTop: "-50px",
+                  marginTop: " -125px",
+                  background: "none",
+                  position: "relative",
+                  zIndex: "999"
+                }}
+              >
+                <div
+                  className="User_prof_img"
+                  style={{
+                    background: "#fff"
+                  }}
+                >
+                  <img src={myInfo.image} />
+                </div>
+                <h2 class="UserName">{myInfo.name}</h2>
+                <div className="My_posts">
+                  <div className="pos_anser">
+                    <span className="float-left">
+                      {" "}
+                      {`Email: ${myInfo.email}`}
+                    </span>
+                    <span className="float-right">{`Phone Number: ${myInfo.phone}`}</span>
+                  </div>
+                  {/* <NewPost /> */}
+                  {/* <div className="pos_anser">My Posts</div> */}
+                  {/* ..........postitem.......... */}
+                  <div>{/* <PostSingle /> */}</div>
+                </div>
+              </div>
+            </div>
+
+            {/* <MyInfo myInfo={myInfo} /> */}
+            <MyPostsList myPosts={myPosts} deletePost={deletePost} />
+          </div>
+        </div>
       </div>
     );
   }
