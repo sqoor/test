cb9ddaf2986d8c51fb946626a0dd79620690911f
Abdullah Alsqoor <alsqoorAbdullah2@gmail.com>
2019-09-18 03:25:23 +0300

    added us route

--- view/src/App.js
+++ view/src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import { Route, Switch, BrowserRouter as Router } from "react-router-dom";
 
 import Home from "./components/home/Home";
-import Contact from "./components/Contact";
+import AboutUs from "./components/AboutUs";
 import NotFound from "./components/NotFound";
 import Register from "./components/register/Register";
 import MyProfile from "./components/profile/MyProfile";
@@ -13,7 +13,7 @@ import { toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 toast.configure({
-  autoClose: 2500,
+  autoClose: 1500,
   draggable: true,
   pauseOnHover: true
 });
@@ -29,7 +29,8 @@ class App extends Component {
   }
 
   userLogged = (value) => {
-    this.setState({ isLogged: !this.state.isLogged })
+    this.setState({ isLogged: value })
+    // this.setState({ isLogged: !this.state.isLogged })
   }
 
   render() {
@@ -42,7 +43,7 @@ class App extends Component {
             <Route path="/profile" component={MyProfile} />
             <Route path="/post/:id" component={PostPage} />
             <Route path="/register" component={() => <Register userLogged={this.userLogged} />} />
-            <Route path="/contact" component={Contact} />
+            <Route path="/us" component={AboutUs} />
             <Route component={NotFound} />
           </Switch>
         </Router>
